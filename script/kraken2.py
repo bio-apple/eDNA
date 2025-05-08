89acf270ecf07b075f3d6f2058e9dd10cb684ac4
yfan <fanyucai3@gmail.com>
2025-05-08 12:46:44 +0800

    no

--- script/kraken2.py
+++ script/kraken2.py
@@ -9,4 +9,5 @@ def run(fa,oudir,prefix,ref):
     if not os.path.exists(outdir):
         subprocess.check_call(f'mkdir -p {outdir}',shell=True)
     cmd=(f'docker -v {os.path.dirname(fa)}:/raw_data/ -v {ref}:/ref/ -v {outdir}:/outdir {docker} sh -c \'export PATH=/opt/conda/envs/edna/bin:$PATH && '
-         f'kraken2 --db /ref/ --threads 24 --output /outdir/%s.txt --report /outdir/%s.report.txt\'')
+         f'kraken2 --db /ref/ --threads 24 --confidence 0.8 --output /outdir/%s.txt --report /outdir/%s.report.txt /raw_data/{fa.split("/")[-1]} && '
+         f' ')
